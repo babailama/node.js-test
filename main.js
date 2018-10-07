9a91eeb361e8615e3b250a5e736d174559db1be1
andivanov@gmail.com <andivanov@gmail.com>
2018-10-07 14:15:58 +0300

    cli arguments

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 var notifier = require('./notifier/notifier.js')
 const Reader = require('./reader/reader');
-var reader = new Reader('input.csv');
+var reader = new Reader(process.argv[2]);
 reader.readData();
 notifier.on('calculate', function (customers) {
     for (var i = 0; i < customers.length; i++) {
-        console.log(customers.get(i).getOrders());
         customers.get(i).calculate();
     }
 });
